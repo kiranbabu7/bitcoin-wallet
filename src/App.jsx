b50fcfa10ae6ebe2b69670ce6a4297bf22e3926b
kiran <kiran@krinati.co>
2024-01-17 20:18:49 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './components/header'
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+
+function Header() {
+    return (
+        <div className="header">
+            <img src="src\assets\chevron-left-solid.svg" alt="" />
+            <h2>Bitcoin Wallet</h2>
+            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="4" viewBox="0 0 128 512">
+                <path fill="#c0c0c0" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
+            </svg>
+        </div>
+    )
+}
+
+export default Header
