3ed2b36939d1c03dd9d0e9de743fe7009c1bd256
Kiran <kiran@krinati.co>
2024-01-18 12:09:19 +0530

    feat: add header and basic crypto wallet UI card

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Wallet from './components/crypto_wallet_card';
 import Header from './components/header'
+
+
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <Wallet name='Bitcoin' cryptoCode='BTC' balance={2.00353453}/>
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,9 +2,11 @@
 function Header() {
     return (
         <div className="header">
-            <img src="src\assets\chevron-left-solid.svg" alt="" />
+            <svg xmlns="http://www.w3.org/2000/svg" height="60px" width="12px" viewBox="0 0 320 512">
+                <path fill="#aeb8c4" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/>
+            </svg>
             <h2>Bitcoin Wallet</h2>
-            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="4" viewBox="0 0 128 512">
+            <svg xmlns="http://www.w3.org/2000/svg" height="60px" width="8px" viewBox="0 0 128 512">
                 <path fill="#c0c0c0" d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
             </svg>
         </div>
